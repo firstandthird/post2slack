93b7f2f9574c58d2743a1f776b8d6325f91dd018
Chris <orthagonal1@gmail.com>
2017-02-23 11:37:30 -0600

    make that thing a class

--- index.js
+++ index.js
@@ -2,74 +2,87 @@
 const _ = require('lodash');
 const Wreck = require('wreck');
 
-module.exports.config = {};
+class Post2Slack {
+  constructor (config) {
+    this.config = config;
+  }
 
-module.exports.post = (payload, done) => {
-  payload = _.isObject(payload) ? JSON.stringify(payload) : payload;
-  Wreck.request('POST', module.exports.config.slackHook, {
-    headers: { 'Content-type': 'application/json' },
-    payload
-  }, done);
-};
+  post (payload, done) {
+    payload = _.isObject(payload) ? JSON.stringify(payload) : payload;
+    Wreck.request('POST', this.config.slackHook, {
+      headers: { 'Content-type': 'application/json' },
+      payload
+    }, (err, response, responsePayload) => {
+      if (err) {
+        return done(err);
+      }
+      if (response.statusCode !== 200) {
+        return done(new Error(`post to ${this.config.slackHook} failed: ${response.statusCode} ${response.statusMessage}`));
+      }
+      return done(null, responsePayload);
+    });
+  }
 
-// used by slackPostMessage to construct a nice payload:
-const makeSlackPayload = (tags, data) => {
-  //clone because we muck with data
-  data = _.cloneDeep(data);
-  const attachment = {
-    fields: []
-  };
+  // used by slackPostMessage to construct a nice payload:
+  makeSlackPayload (tags, data) {
+    //clone because we muck with data
+    data = _.cloneDeep(data);
+    const attachment = {
+      fields: []
+    };
 
-  if (_.isString(data)) { //if string just pass as title and be done with it
-    attachment.title = data;
-    attachment.fallback = data;
-  } else if (_.isObject(data)) { // if object, then lets make it look good
-    //if it has a message, pull that out and display as title
-    if (data.message) {
-      attachment.title = data.message;
-      attachment.fallback = data.message;
-      delete data.message;
+    if (_.isString(data)) { //if string just pass as title and be done with it
+      attachment.title = data;
+      attachment.fallback = data;
+    } else if (_.isObject(data)) { // if object, then lets make it look good
+      //if it has a message, pull that out and display as title
+      if (data.message) {
+        attachment.title = data.message;
+        attachment.fallback = data.message;
+        delete data.message;
+      }
+      if (data.url) {
+        attachment.title_link = data.url;
+        delete data.url;
+      }
+      attachment.text = `\`\`\` ${JSON.stringify(data, null, '  ')} \`\`\``;
+      attachment.mrkdwn_in = ['text'];
     }
-    if (data.url) {
-      attachment.title_link = data.url;
-      delete data.url;
+    if (this.config.additionalFields) {
+      attachment.fields = attachment.fields.concat(this.config.additionalFields);
     }
-    attachment.text = `\`\`\` ${JSON.stringify(data, null, '  ')} \`\`\``;
-    attachment.mrkdwn_in = ['text'];
+    if (this.config.hideTags !== true && tags.length > 0) {
+      attachment.fields.push({ title: 'Tags', value: tags.join(', ') });
+    }
+    // set any colors for special tags:
+    if (tags.indexOf('success') > -1) {
+      attachment.color = 'good';
+    }
+    if (tags.indexOf('warning') > -1) {
+      attachment.color = 'warning';
+    }
+    if (tags.indexOf('error') > -1) {
+      attachment.color = 'danger';
+    }
+    // set any special channel:
+    const slackPayload = {
+      attachments: [attachment]
+    };
+    if (this.config.channel) {
+      slackPayload.channel = this.config.channel;
+    }
+    if (this.config.iconURL) {
+      slackPayload.icon_url = this.config.iconURL;
+    }
+    if (this.config.username) {
+      slackPayload.username = this.config.username;
+    }
+    return JSON.stringify(slackPayload);
   }
-  if (module.exports.config.additionalFields) {
-    attachment.fields = attachment.fields.concat(module.exports.config.additionalFields);
+  // will format and slackPostRawMessage a server.log style message to slack:
+  postFormatted(tags, message, done) {
+    this.post(this.makeSlackPayload(tags, message), done);
   }
-  if (module.exports.config.hideTags !== true && tags.length > 0) {
-    attachment.fields.push({ title: 'Tags', value: tags.join(', ') });
-  }
-  // set any colors for special tags:
-  if (tags.indexOf('success') > -1) {
-    attachment.color = 'good';
-  }
-  if (tags.indexOf('warning') > -1) {
-    attachment.color = 'warning';
-  }
-  if (tags.indexOf('error') > -1) {
-    attachment.color = 'danger';
-  }
-  // set any special channel:
-  const slackPayload = {
-    attachments: [attachment]
-  };
-  if (module.exports.config.channel) {
-    slackPayload.channel = module.exports.config.channel;
-  }
-  if (module.exports.config.iconURL) {
-    slackPayload.icon_url = module.exports.config.iconURL;
-  }
-  if (module.exports.config.username) {
-    slackPayload.username = module.exports.config.username;
-  }
-  return JSON.stringify(slackPayload);
-};
+}
 
-// will format and slackPostRawMessage a server.log style message to slack:
-module.exports.postFormatted = (tags, message, done) => {
-  module.exports.post(makeSlackPayload(tags, message), done);
-};
+module.exports = (config) => new Post2Slack(config);
--- test/post.test.js
+++ test/post.test.js
@@ -1,7 +1,6 @@
 'use strict';
 const tape = require('tape');
 const tapeExtra = require('tape-extras');
-const post2slack = require('../index.js');
 const Hapi = require('hapi');
 
 let server;
@@ -15,22 +14,55 @@ const test = tapeExtra(tape, {
   }
 });
 
+test('creates a post2slack class instance', (t) => {
+  const post2slack = require('../index.js')({});
+  t.equal(typeof post2slack.post, 'function', 'has "post" function');
+  t.equal(typeof post2slack.postFormatted, 'function', 'has "postFormatted" function');
+  t.end();
+});
 
-test('sends a message', (t) => {
+test('sends a raw message', (t) => {
   // set up a temp server:
+  const post2slack = require('../index.js')({ slackHook: 'http://localhost:8080/test1' });
   server.route({
     method: 'POST',
-    path: '/',
-    handler: (req, reply) => {
-      t.equal(req.payload.thing, 'is a thing', 'will post a raw package to slack');
+    path: '/test1',
+    handler: (request, reply) => {
+      t.equal(request.payload.thing, 'is a thing', 'will post a raw package to slack');
+      reply({ payload: 'yeah' });
+    }
+  });
+  post2slack.post({ thing: 'is a thing' }, (err, result) => {
+    t.equal(err, null, 'does not error by default');
+    server.stop(t.end);
+  });
+});
+
+test('sends a formatted message', (t) => {
+  const post2slack = require('../index.js')({ slackHook: 'http://localhost:8080/test2' });
+  server.route({
+    method: 'POST',
+    path: '/test2',
+    handler: (request, reply) => {
+      t.equal(Array.isArray(request.payload.attachments), true, 'attachments is an array');
+      t.equal(request.payload.attachments[0].text.indexOf('is another thing') > -1, true, 'text field is set up correctly');
+      t.equal(request.payload.attachments[0].fields[0].title, 'Tags', 'tag fields set up correctly');
+      t.equal(request.payload.attachments[0].fields[0].value, 'aTag, anotherTag', 'tag fields set up correctly');
+      t.equal(request.payload.attachments[0].mrkdwn_in[0], 'text', 'tag fields set up correctly');
       reply({});
     }
   });
-  post2slack.post('http://localhost:8080', { thing: 'is a thing' }, (err, result) => {
-    console.log('does this call?')
+  post2slack.postFormatted(['aTag', 'anotherTag'], { thing: 'is another thing' }, (err, result) => {
     t.equal(err, null, 'does not error by default');
-    console.log(result.result)
-    // t.equal(result.payload, , 'does not error by default');
+    server.stop(t.end);
+  });
+});
+
+test('returns errors', (t) => {
+  const post2slack = require('../index.js')({ slackHook: 'http://localhost:8080/' });
+  post2slack.postFormatted(['aTag', 'anotherTag'], { thing: 'is another thing' }, (err, result) => {
+    t.notEqual(err, null, 'returns an error');
+    t.equal(err.toString(), 'Error: post to http://localhost:8080/ failed: 404 Not Found');
     server.stop(t.end);
   });
 });
