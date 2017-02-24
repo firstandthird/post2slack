558bc12ad8d11f662df4171b159c9fe49952900d
Chris <orthagonal1@gmail.com>
2017-02-23 18:17:47 -0600

    iconEmoji

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ class Post2Slack {
     if (this.config.iconURL) {
       slackPayload.icon_url = this.config.iconURL;
     }
-    if (this.config.icon_emoji) {
-      slackPayload.icon_emoji = this.config.icon_emoji;
+    if (this.config.iconEmoji) {
+      slackPayload.icon_emoji = this.config.iconEmoji;
     }
     if (this.config.username) {
       slackPayload.username = this.config.username;
